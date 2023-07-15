abc5fdd425c256398031827f255b698f15ee5ad4
Kalinduadikari <kalindukadikari@gmail.com>
2023-07-15 07:26:49 +0530

    ready to deploy

--- src/services/mlModelService.js
+++ src/services/mlModelService.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "https://ofsosnode-api.onrender.com/api";
-
+const BACKEND_URL = process.env.REACT_APP_OFSOS_BACKEND_URL;
 const API_URL = `${BACKEND_URL}/mlmodel/`;
 
 //GET BEST SELLING PRODUCTS COMBINATIONS
